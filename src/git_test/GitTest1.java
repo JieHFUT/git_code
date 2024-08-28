9f2fd77aa05b993b9306576a19749e19e7d26bfe
JieCHERY <3492779706@qq.com>
2024-08-28 09:11:31 +0800

    this is first commit the hot-fix branch

--- src/git_test/GitTest1.java
+++ src/git_test/GitTest1.java
@@ -7,6 +7,7 @@ public class GitTest1 {
 
         System.out.println("这是学习测试 git 第一次提交内容");
         System.out.println("这是学习测试 git 第二次提交内容");
+        System.out.println("this is hot-fix branch");
 
 
     }
